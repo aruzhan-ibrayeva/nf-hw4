c1d2f314f5cafe7ec659eefc3fd8b998f610fe92
Aruzhan <aruzhan.s.ibrayeva@gmail.com>
2024-06-10 13:10:37 +0500

    little fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-// pages/index.tsx
 import { useQuery } from 'react-query';
 import { fetchProducts } from '@/src/services/productService';
 import ProductCard from '../src/components/ProductCard';
+import { Product } from '@/src/types/product';
 
 export default function HomePage() {
     const { data: products, isLoading, error } = useQuery('products', fetchProducts);
@@ -13,7 +13,7 @@ export default function HomePage() {
         <div>
             <h1>Welcome to the Products App</h1>
             <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                {products?.map(product => (
+                {products?.map((product: Product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
             </div>
